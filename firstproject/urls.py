3ccc4058e3e6e6ad47fee5ebfb9f3cdf9590ad18
leetaerim <ltr0121@naver.com>
2019-06-07 23:16:00 +0900

    word count bootstrap

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', wordcount.views.home, name='home'),
     path('about/', wordcount.views.about, name='about'),
     path('count/', wordcount.views.count, name='count'),
+    
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def home(request):
-    return render(request, 'wordcount/home.html')
+    return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'wordcount/about.html')
+    return render(request, 'about.html')
 
 def count(request):
     full_text = request.GET['fulltext']
@@ -18,4 +17,4 @@ def count(request):
         else:
             word_dictionary[word] = 1
 
-    return render(request, 'wordcount/count.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
+    return render(request, 'count.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
